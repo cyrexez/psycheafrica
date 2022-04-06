7e370f8aa1d58365f26d5d42aefdfd5b1b56ee33
tsherrifdeen <43505184+tsherrifdeen@users.noreply.github.com>
2022-04-07 00:13:57 +0100

    Update footer.vue

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,8 +1,13 @@
 <template>
 <JoinUs/>
 <div class="footer">
-  <div class="flex">
-    <p> All rights reserved</p> <img src="../assets/img/bi_linkedin.png"><img src="../assets/img/fa-brands_twitter.png"><img src="../assets/img/facebook.png">
+  <div class="">
+    <p>  {{ year }} All rights reserved</p>
+    <div>  
+      <img src="../assets/img/bi_linkedin.png">
+      <img src="../assets/img/fa-brands_twitter.png">
+      <img src="../assets/img/facebook.png">
+    </div>
   </div>
 </div>
 
@@ -11,15 +16,19 @@
 <script>
 import JoinUs from '../components/JoinUs.vue' 
 export default {
-  name: 'Footer',
   components: {JoinUs},
   props: {
     msg: String
+  },
+  setup() {
+    const Year = new Date();
+    Year.getFullYear();
+    return { year }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  
+
 </style>
