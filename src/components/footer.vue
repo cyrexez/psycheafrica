32d849bf4289d9651c1cddf0b20920b70fe1b2dc
tsherrifdeen <43505184+tsherrifdeen@users.noreply.github.com>
2022-04-07 00:56:59 +0100

    Update footer.vue

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,14 +1,14 @@
 <template>
 <JoinUs/>
 <div class="footer">
-  <div class="">
-    <p>  {{ year }} All rights reserved</p>
+  <div>
+    <p>{{ year }} 2022 Psyche Africa . All rights reserved</p>
+  </div>
     <div>  
       <img src="../assets/img/bi_linkedin.png">
       <img src="../assets/img/fa-brands_twitter.png">
       <img src="../assets/img/facebook.png">
     </div>
-  </div>
 </div>
 
 </template>
@@ -17,18 +17,23 @@
 import JoinUs from '../components/JoinUs.vue' 
 export default {
   components: {JoinUs},
-  props: {
-    msg: String
-  },
-  setup() {
-    const Year = new Date();
-    Year.getFullYear();
-    return { year }
-  }
+  // props: {
+  //   msg: String
+  // },
+  // setup() {
+  //   const Year = new Date();
+  //   Year.getFullYear();
+  //   return { year }
+  // }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+  .footer {
+    border-top: 1px solid white;
+    display: flex;
+    justify-content: space-between;
+    padding: 10px 80px;
+  }
 </style>
