231e1acf7170645393ce150ba801b6f58f95b56a
tsherrifdeen <43505184+tsherrifdeen@users.noreply.github.com>
2022-04-17 16:13:01 +0100

    Update otp.vue

--- src/views/otp.vue
+++ src/views/otp.vue
@@ -3,17 +3,17 @@
     <template #heading>
         Enter your o.t.p
       <div class="enter-pw">
-        A 6-digit Code has been sent to your Email {{ email}}. Enter the OTP Number below  
+        A 6-digit Code has been sent to your Email {{ email }}. Enter the OTP Number below  
       </div>
     </template>
     <template #form>
-       <div style="display: flex; flex-direction: row;">
+       <div class="otp-div">
           <VOtpInput ref="otpInput" input-classes="otp-input" separator=" " :num-inputs="6" :should-auto-focus="true" :is-input-num="true" @on-change="handleOnChange" @on-complete="handleOnComplete"/>
         </div>
     </template>
-    <template #after-btn>
-        <p>Didn't get an email?</p>
-        <a href="">Click here to resend</a>
+    <template #afterBtn>
+        <p class="resend-div">Didn't get an email? <br>
+        <a href="">Click here to resend</a> </p>
     </template>
   </LoginTemp>
 </template>
@@ -32,6 +32,7 @@ export default {
     TextInput,
     VOtpInput,
   },
+
   setup() {
     const otpInput = ref(null)
 
@@ -52,8 +53,7 @@ export default {
 }
 </script>
 
-<style scoped>
-
+<style>
 .enter-pw {
   margin-top: 15px;
   padding:0 15px;
@@ -63,11 +63,17 @@ export default {
   text-align: center;
   letter-spacing: -0.24px;
 }
+.otp-div {
+  display: flex; 
+  width:100%;  
+  flex-direction: row;
+  margin: 24px 0 50px;
+}
 .otp-input {
-  width: 20px;
-  height: 40px;
+  width: 41px;
+  height: 45px;
   padding: 5px;
-  margin: 24px 0;
+  margin: 0 4px;
   font-size: 16px;
   border-radius: 4px;
   border: 1px solid rgba(0, 0, 0, 0.3);
@@ -75,9 +81,17 @@ export default {
 }
 .otp-input:focus {
   outline: none;
+  border: 1px solid lightblue;
 }
-button[type='submit'] {
+/* button[type='submit'] {
   display: none !important;
+} */
+.resend-div {
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 22px;
+  letter-spacing: -0.23999999463558197px;
+  text-align: center;
+  color: #595959;
 }
-
 </style>
